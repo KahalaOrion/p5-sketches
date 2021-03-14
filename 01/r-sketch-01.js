4814dc3c128cba62bda4616d5b80461f9460598c
KahalaOrion <78617741+KahalaOrion@users.noreply.github.com>
2021-03-14 17:17:00 -0400

    R sketch 01 done

--- 01/r-sketch-01.js
+++ 01/r-sketch-01.js
@@ -1,6 +1,7 @@
 function setup() {
 	var c = createCanvas(1000,1000);
 	print("setup function!")
+	angleMode(DEGREES);
 }
 
 function draw(){
@@ -8,17 +9,19 @@ function draw(){
 	noStroke (255);
 
 
-		// (x,y,width,height)
+	// rect(x,y,width,height)
+	// arc(x, y, w, h, startAngle, stopANgle, [mode], [detail])
+	// line(x1, y1, x2, y2)
 
-	rect(250,200,100,600);
+	rect(250,190,110,640);
 	noFill();
 	stroke (0);
-	strokeWeight(80);
+	strokeWeight(100);
 	strokeCap(PROJECT);
-	arc(300, 400, 350, 350, 20,10, PI + QUARTER_PI, OPEN);
+	arc(410, 377, 350, 275, 270, 90);
 	// ellipse(500,400,350,350);
 	
-	line(380, 570, 590, 750);
+	line(500, 550, 620, 760);
 	// rect(250,300,100,350)
 	// fill(255)
 	// ellipse (260,230,75,75)
@@ -36,6 +39,6 @@ function draw(){
 
 }
 
-// function mousePressed() {
-// 	saveCanvas("sketch-01","png")
-// }
+function mousePressed() {
+	saveCanvas("sketch-01","png")
+}
--- 02/r-sketch-02.js
+++ 02/r-sketch-02.js
@@ -1,41 +1,18 @@
 function setup() {
-	var c = createCanvas(1000,1000);
 	print("setup function!")
+	var c = createCanvas(1000,1000);
+	background(0)
 }
 
 function draw(){
-	fill (0);
-	noStroke (255);
-
-
-		// (x,y,width,height)
-
-	rect(250,200,100,600);
-	noFill();
-	stroke (0);
-	strokeWeight(80);
-	strokeCap(PROJECT);
-	arc(300, 400, 350, 350, 20,10, PI + QUARTER_PI, OPEN);
-	// ellipse(500,400,350,350);
-	
-	line(380, 570, 590, 750);
-	// rect(250,300,100,350)
-	// fill(255)
-	// ellipse (260,230,75,75)
-
-	// var i = 0;
-	// stroke(0)
-	// while (i<100) {
-
-	// 		ellipse(10*i,10*i,250,250)
-
-	// 	i=i+1;
-	// }
-
-
+// (x,y,width,height)
+stroke(250)
+strokeWeight(1)
+line(mouseX, mouseY, 500, 500)
 
 }
+	
 
 // function mousePressed() {
-// 	saveCanvas("sketch-01","png")
+// 	saveCanvas("sketch-02","png")
 // }
