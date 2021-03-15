fd11e4bf00894606d9a111b15845a4e431618745
KahalaOrion <78617741+KahalaOrion@users.noreply.github.com>
2021-03-15 17:54:51 -0400

    sketch 4 done

--- 04/r-sketch-04.js
+++ 04/r-sketch-04.js
@@ -1,31 +1,53 @@
-// Following The Coding Train 2.2: Variables in p5.js (Define Your Own) - p5.js Tutorial
+// Following The Coding Train 2.2: Variables in p5.js (Define Your Own) - p5.js Tutorial + 2.4: random() Function - p5.js Tutorial
 
 
 
-let circleX = 100;
+let circleSize;
+let rectSize
+let lineWidth;
+let strokeColor;
+let legWidth;
+
 
 function setup() {
-	createCanvas(1000,1000);
 	print("setup function!")
-	angleMode(DEGREES);
+	createCanvas(1000,1000);
+	background(255);
+	
+	// angleMode(DEGREES);
 }
 
-function mousePressed(){
-	circleX = 0;
-}
+// function mousePressed(){
+// 	circleX = 100;
+// }
 
 
 function draw(){
-	fill (0);
-	noStroke (255);
 
-
+	circleSize = random (5,350);
+	lineWidth = random(2,10);
+	strokeColor = random(0,255);
 	
+	strokeWeight(lineWidth);
+	stroke(strokeColor);
+	noFill();
+	circle(500, 400, circleSize);
 
+	rectSize = random(5,665)
+
+	rect(250,215,150,rectSize);
+
+
+	legWidth = random (1,150)
+
+	strokeCap(PROJECT);
+	strokeWeight(legWidth);
+	stroke(strokeColor)
+	line(440, 620, 620, 775);
 
 
 }
 
-// function mousePressed() {
-// 	saveCanvas("sketch-01","png")
-// }
+function mousePressed() {
+	saveCanvas("sketch-04","png")
+}
